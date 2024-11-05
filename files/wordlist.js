3add2cd5b45f2aac9008f1e0d2d5840c4a168ad4
Reuven Reyman <94356206+reuvenrey@users.noreply.github.com>
2024-11-05 15:27:29 -0800

    Create wordlist.js

---
+++ files/wordlist.js
@@ -0,0 +1,4 @@
+clueList = ["Disneyland", "Steak House", "Corner Bakery", "Audi", "Fresno", "Irvine Spectrum", "Pumpkin", "Jake Peralta", "Amy Santiago", "Captain Holt", "Spider",  \
+           "Old Man", "Running Shoe", "Jockstrap", "Rave", "Hardstyle", "Bedroom", "clue", "clue", "clue", "clue", "clue",  \
+           "clue", "clue", "clue", "clue", "clue", "clue", "clue", "clue", "clue", "clue", "clue"];
+
