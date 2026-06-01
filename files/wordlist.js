ac34438db4cc4546cdc37300ffb008c9edc5db7f
Reuven Reyman <94356206+reuvenrey@users.noreply.github.com>
2026-06-01 14:18:31 -0700

    Update wordlist.js

--- files/wordlist.js
+++ files/wordlist.js
@@ -1,2 +1,2 @@
-clueList = ["Dog","Islands","Beamer","Painting","Aerospace","Rocket","Satelite","Airplane","Lego","Red Velvet Cake","Minecraft","Honda Civic",""];
+clueList = ["Dog","Islands","Beamer","Painting","Aerospace","Rocket","Satelite","Airplane","Lego","Red Velvet Cake","Minecraft","Honda Civic","Great British Baking Show","Biscoff Cookie","Pretty Princess", "Pretty Titties", "Bubble Bath"];
 
